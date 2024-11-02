41ab844d4762ee1105456d78bc1b3663ded05507
NikitaBarkalov <nekitmga126@gmail.com>
2024-11-02 22:40:53 +0200

    * Overriding method for updating quality in a   class AgedBrie

--- src/lab3/main/AgedBrie.java
+++ src/lab3/main/AgedBrie.java
@@ -4,4 +4,12 @@ public class AgedBrie extends Item {
     public AgedBrie(int sellIn, int quality) {
         super("Aged Brie", sellIn, quality);
     }
+
+    @Override
+    public int updateQuality() {
+        if (sellIn > 0) {
+            return QualityValidator.validateNotMoreThan50Quality(++quality);
+        }
+        return QualityValidator.validateNotMoreThan50Quality(quality + 2);
+    }
 }
