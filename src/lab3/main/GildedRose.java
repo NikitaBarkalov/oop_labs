5ac3f7215054b38d21d2b0bfabdb565ddee9e48b
NikitaBarkalov <nekitmga126@gmail.com>
2024-11-02 23:42:29 +0200

    * Changed a for-cycle by indexes to for-each because indexes wasn't used in any different place in a code

--- src/lab3/main/GildedRose.java
+++ src/lab3/main/GildedRose.java
@@ -8,8 +8,8 @@ public class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            items[i].updateParameters();
+        for (Item item: items) {
+            item.updateParameters();
         }
     }
 }
