ed20cff794f24e6069b10040ce438f0985352699
NikitaBarkalov <nekitmga126@gmail.com>
2024-11-02 22:36:49 +0200

    * Overriding methods for updating parameters in a   class Sulfuras

--- src/lab3/main/Sulfuras.java
+++ src/lab3/main/Sulfuras.java
@@ -4,4 +4,19 @@ public class Sulfuras extends Item {
     public Sulfuras(int sellIn) {
         super("Sulfuras, Hand of Ragnaros", sellIn, 80);
     }
+
+    @Override
+    public int updateQuality() {
+        return quality;
+    }
+
+    @Override
+    public int updateSellIn() {
+        return sellIn;
+    }
+
+    @Override
+    public void updateParameters() {
+        return;
+    }
 }
