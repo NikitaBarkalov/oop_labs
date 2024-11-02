f01dbaec60b7917e4c48a6ea953b014a7e2712d1
NikitaBarkalov <nekitmga126@gmail.com>
2024-11-02 23:18:07 +0200

    * Overriding a method for updating a quality in a   class ConjuredManaCake

--- src/lab3/main/ConjuredManaCake.java
+++ src/lab3/main/ConjuredManaCake.java
@@ -4,4 +4,12 @@ public class ConjuredManaCake extends Item {
     public ConjuredManaCake(int sellIn, int quality) {
         super("Conjured Mana Cake", sellIn, quality);
     }
+
+    @Override
+    public int updateQuality() {
+        if (sellIn > 0) {
+            return QualityValidator.validateNotNegativeQuality(quality - 2);
+        }
+        return QualityValidator.validateNotNegativeQuality(quality - 4);
+    }
 }
