e86bd0e3d38e58bd710389ff0390df503654eb93
NikitaBarkalov <nekitmga126@gmail.com>
2024-11-02 22:47:36 +0200

    * Overriding a method for updating qualities in a   class BackstagePasses using private methods for   updating it in different cases

--- src/lab3/main/BackastagePasses.java
+++ src/lab3/main/BackastagePasses.java
@@ -4,4 +4,41 @@ public class BackastagePasses extends Item {
     public BackastagePasses(int sellIn, int quality) {
         super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
     }
+
+    @Override
+    public int updateQuality() {
+        quality = updateQualityWhenSellInMoreThan10();
+        quality = updateQualityWhenSellInBetween5And10();
+        quality = updateQualityWhenSellInBetween0And5();
+        quality = updateQualityWhenSellInLessThan0();
+        return quality;
+    }
+
+    private int updateQualityWhenSellInMoreThan10() {
+        if (sellIn > 10) {
+            return QualityValidator.validateNotMoreThan50Quality(++quality);
+        }
+        return quality;
+    }
+
+    private int updateQualityWhenSellInBetween5And10() {
+        if (sellIn > 5 && sellIn <= 10) {
+            return QualityValidator.validateNotMoreThan50Quality(quality + 2);
+        }
+        return quality;
+    }
+
+    private int updateQualityWhenSellInBetween0And5() {
+        if (sellIn > 0 && sellIn <= 5) {
+            return QualityValidator.validateNotMoreThan50Quality(quality + 3);
+        }
+        return quality;
+    }
+
+    private int updateQualityWhenSellInLessThan0() {
+        if (sellIn <= 0) {
+            return 0;
+        }
+        return quality;
+    }
 }
