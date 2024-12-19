fc6188d3967a2c73ac6490c6da50bb0ace46de3b
NikitaBarkalov <nekitmga126@gmail.com>
2024-12-19 13:07:46 +0200

    Updated classes for Compression

--- src/lab5/main/java/compression/Compression.java
+++ src/lab5/main/java/compression/Compression.java
@@ -3,5 +3,5 @@ package lab5.main.java.compression;
 import lab5.main.java.image.*;
 
 public interface Compression {
-    public double executeCompressing(Image image);
+    public Image executeCompressing(Image image);
 }
--- src/lab5/main/java/compression/LosslessCompression.java
+++ src/lab5/main/java/compression/LosslessCompression.java
@@ -4,12 +4,13 @@ import lab5.main.java.image.Image;
 
 public class LosslessCompression implements Compression {
     @Override
-    public double executeCompressing(Image image) {
+    public Image executeCompressing(Image image) {
         double sizeMemory = image.getSizeMemory();
         if (image.isImageCompressed()) {
-            return sizeMemory;
+            return image;
         }
         image.setNewCompressingStatus(true);
-        return sizeMemory * 0.5;
+        image.setNewSizeMemory(sizeMemory * 0.5);
+        return image;
     }
 }
