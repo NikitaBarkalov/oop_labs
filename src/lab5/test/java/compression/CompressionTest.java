6809b3d6e872fbc7e86f01c405949df0f4847429
NikitaBarkalov <nekitmga126@gmail.com>
2024-12-19 13:07:20 +0200

    Updated tests for Compression

--- src/lab5/test/java/compression/CompressionTest.java
+++ src/lab5/test/java/compression/CompressionTest.java
@@ -10,33 +10,49 @@ public class CompressionTest {
     public void whenUsingLosslessCompression_thenSizeDecreasingBy50Percents() {
         Image image1 = new Image("photo1", 960, 1080, 1);
         Compression compression1 = new LosslessCompression();
-        assertEquals(0.5, compression1.executeCompressing(image1));
+        assertEquals(0.5, compression1.executeCompressing(image1).getSizeMemory());
     }
 
     @Test
     public void whenUsingLossyCompression_thenSizeDecreasingBy80percents() {
         Image image1 = new Image("photo1", 960, 1080, 1);
         Compression compression1 = new LossyCompression();
-        assertEquals(0.2, compression1.executeCompressing(image1));
+        assertEquals(0.2, compression1.executeCompressing(image1).getSizeMemory());
     }
 
     @Test
-    public void whenImageAlreadyCompressed_thenLosslessCompressionIsNotChangedSizeMemory() {
+    public void whenImageAlreadyCompressedByLosslessCompression_thenLosslessCompressionIsNotChangedSizeMemory() {
         Image image1 = new Image("photo1", 960, 1080, 1);
         Compression compression1 = new LosslessCompression();
-        double newSize = compression1.executeCompressing(image1);
-        image1.setNewSizeMemory(newSize);
+        compression1.executeCompressing(image1);
         Compression compression2 = new LosslessCompression();
-        assertEquals(0.5, compression2.executeCompressing(image1));
+        assertEquals(0.5, compression2.executeCompressing(image1).getSizeMemory());
     }
 
     @Test
-    public void whenImageAlreadyCompressed_thenLossyCompressionIsNotChangedSizeMemory() {
+    public void whenImageAlreadyCompressedByLossyCompression_thenLossyCompressionIsNotChangedSizeMemory() {
         Image image1 = new Image("photo1", 960, 1080, 1);
         Compression compression1 = new LossyCompression();
-        double newSize = compression1.executeCompressing(image1);
-        image1.setNewSizeMemory(newSize);
+        compression1.executeCompressing(image1);
         Compression compression2 = new LossyCompression();
-        assertEquals(0.2, compression2.executeCompressing(image1));
+        assertEquals(0.2, compression2.executeCompressing(image1).getSizeMemory());
+    }
+
+    @Test
+    public void whenImageAlreadyCompressedByLossyCompression_thenLosslessCompressionIsNotChangedSizeMemory() {
+        Image image1 = new Image("photo1", 960, 1080, 1);
+        Compression compression1 = new LossyCompression();
+        compression1.executeCompressing(image1);
+        Compression compression2 = new LosslessCompression();
+        assertEquals(0.2, compression2.executeCompressing(image1).getSizeMemory());
+    }
+
+    @Test
+    public void whenImageAlreadyCompressedByLosslessCompression_thenLossyCompressionIsNotChangedSizeMemory() {
+        Image image1 = new Image("photo1", 960, 1080, 1);
+        Compression compression1 = new LosslessCompression();
+        compression1.executeCompressing(image1);
+        Compression compression2 = new LossyCompression();
+        assertEquals(0.5, compression2.executeCompressing(image1).getSizeMemory());
     }
 }
