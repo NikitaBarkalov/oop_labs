dd89d7ff60771a21e7c43ba4917a3ff04665b77a
NikitaBarkalov <nekitmga126@gmail.com>
2024-12-19 23:04:25 +0200

    Added tests with checking validating size

--- src/lab5/test/java/decorator/SizeDecoratorTest.java
+++ src/lab5/test/java/decorator/SizeDecoratorTest.java
@@ -13,4 +13,52 @@ public class SizeDecoratorTest {
         assertEquals(900, image1.getWidth());
         assertEquals(1000, image1.getHeight());
     }
+
+    @Test
+    public void whenWidthIsNotPositive_thenWidthIsNotUpdated() {
+        GraphicFile image1 = new Image("photo1", 960, 1080, 1);
+        image1 = new SizeDecorator(image1, -900, 1000);
+        assertEquals(960, image1.getWidth());
+        assertEquals(1000, image1.getHeight());
+    }
+
+    @Test
+    public void whenHeightIsNotPositive_thenHeightIsNotUpdated() {
+        GraphicFile image1 = new Image("photo1", 960, 1080, 1);
+        image1 = new SizeDecorator(image1, 900, -1000);
+        assertEquals(900, image1.getWidth());
+        assertEquals(1080, image1.getHeight());
+    }
+
+    @Test
+    public void whenWidthAndHeightIsNotPositive_thenSizeIsNotUpdated() {
+        GraphicFile image1 = new Image("photo1", 960, 1080, 1);
+        image1 = new SizeDecorator(image1, -900, -1000);
+        assertEquals(960, image1.getWidth());
+        assertEquals(1080, image1.getHeight());
+    }
+
+    @Test
+    public void whenHeightIsZero_thenHeightIsNotUpdated() {
+        GraphicFile image1 = new Image("photo1", 960, 1080, 1);
+        image1 = new SizeDecorator(image1, 900, 0);
+        assertEquals(900, image1.getWidth());
+        assertEquals(1080, image1.getHeight());
+    }
+
+    @Test
+    public void whenWidthIsZero_thenHeightIsNotUpdated() {
+        GraphicFile image1 = new Image("photo1", 960, 1080, 1);
+        image1 = new SizeDecorator(image1, 0, 1000);
+        assertEquals(960, image1.getWidth());
+        assertEquals(1000, image1.getHeight());
+    }
+
+    @Test
+    public void whenBothWidthAndHeightAreZero_thenSizeIsNotUpdated() {
+        GraphicFile image1 = new Image("photo1", 960, 1080, 1);
+        image1 = new SizeDecorator(image1, 0, 0);
+        assertEquals(960, image1.getWidth());
+        assertEquals(1080, image1.getHeight());
+    }
 }
